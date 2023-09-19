bdfd8af2b267f821d9ba50729936ec2cacfe610a
Adam Fischer <38475224+f1schhh@users.noreply.github.com>
2023-09-19 16:12:55 +0200

    Uppdatering
    Uppdatering

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
   $("body").css("opacity", "0");
   $("body").animate({ opacity: '1' }, 1000);
+
+  $(".mobile_btn").on('click', function () {
+    $(".mobile_btn").hide();
+    $('#mobile_menu').fadeToggle(300);
+  });
+  $(".close_mobile_btn").on('click', function () {
+    $(".mobile_btn").show();
+    $('#mobile_menu').fadeToggle(300);
+  });
+
 });
+
+function resetMenu() {
+  $("#mobile_menu").hide();
+  $(".mobile_btn").show();
+}
